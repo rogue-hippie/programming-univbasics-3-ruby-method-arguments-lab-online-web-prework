f9a65a2e93c58236ad248d971a60c2e930cf7f5f
funny-array-8950 <hodgkins.whitney@gmail.com>
2019-10-13 03:45:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,7 +15,7 @@ end
 
 introduction_with_language("Whitney", "Ruby")
 
-def introduction_with_language_optional(name)
+def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in "
 
 
